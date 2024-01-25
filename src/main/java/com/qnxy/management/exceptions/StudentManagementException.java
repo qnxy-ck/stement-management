7f79bf995cb2019f53586ff2d213acb25fc70826
qnxy <1632009255@qq.com>
2024-01-25 12:26:11 +0800

    增加学生信息管理异常信息

---
+++ src/main/java/com/qnxy/management/exceptions/StudentManagementException.java
@@ -0,0 +1,14 @@
+package com.qnxy.management.exceptions;
+
+/**
+ * 学生信息管理相关异常
+ *
+ * @author Qnxy
+ */
+public class StudentManagementException extends RuntimeException {
+
+    public StudentManagementException(String message) {
+        super(message);
+    }
+
+}
